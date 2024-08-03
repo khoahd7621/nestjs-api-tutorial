d40a9fc1738fdb3fb1e9a3f127f45f426ef87e2b
khoahd7621 <hoangdangkhoa7621@gmail.com>
2024-08-03 20:35:40 +0700

    NestJS 101 FreeCodeCamp

---
+++ src/user/user.controller.ts
@@ -0,0 +1,24 @@
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
+
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+  @HttpCode(HttpStatus.OK)
+  @Get('me')
+  getMe(@GetUser() user: User) {
+    return user;
+  }
+
+  @Patch()
+  editUser() {}
+}
